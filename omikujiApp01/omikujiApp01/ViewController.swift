0f9bf30e3a8129fd9bc473b4aa6d01c8a26da5b3
Boku <boku.ios.dev@gmail.com>
2020-01-08 02:04:47 +0900

    storybord作成

---
+++ omikujiApp01/omikujiApp01/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  omikujiApp01
+//
+//  Created by 鶴尾直幸 on 2020/01/08.
+//  Copyright © 2020 Tsuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func shuffleButton(_ sender: Any) {
+        
+    }
+}
+
